ddc48eab0415d0cf76dcc87bed06b02708d8e516
NgoHoangMinh2002 <minhngo1409@gmail.com>
2024-05-08 20:49:50 +0700

    Lesson2

---
+++ Lesson02_NHM/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lesson02_NHM.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products ~ http://localhost:port/Products
+        public ActionResult Index()
+        {
+            ViewBag.name = "Ngo Minh - 2210900044";
+            return View();
+        }
+        public ActionResult Details(int? id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+        public string Getname()
+        {
+            return "Ngo Hoang Minh - 2210900044";
+        }
+        public JsonResult listname()
+        {
+            string[] name = { "Minh", "Long", "Nam", "KHACDUY" };
+
+            return Json(name, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
